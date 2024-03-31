b3b0564a1c4879078325a3974b962a031b2b8997
kerimovok <naxro.ok1@gmail.com>
2024-03-31 19:48:38 +0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    // SCROLL TO TOP
+    var windowH = $(window).height() / 2;
+
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > windowH) {
+            $("#scroll-up").css("opacity", "1");
+        } else {
+            $("#scroll-up").css("opacity", "0");
+        }
+    });
+});
